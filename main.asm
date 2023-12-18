ee9cc8fe64b5f3c226c9a2215a1c1f61bd17e6ec
Joshua F <97619681+JoshuaFarmer@users.noreply.github.com>
2023-12-18 15:09:19 +0000

    Update main.asm

--- main.asm
+++ main.asm
@@ -1,51 +1,103 @@
 #fasm#         ; use flat assembler syntax
 #make_boot#
-org 7c00h      ; set location counter.    
- 
+org 0x7C00
 
-; initialize the stack:
-mov     ax, 07c0h
-mov     ss, ax
-mov     sp, 03feh ; top of the stack.
+; Bootloader By HighBit.
 
+; We need a BPB, since some BIOS'es override this section of memory.
+        jmp _start
+        nop
+        
+        sector_bytes: dw 512
+        cluster_sects: db 0
+        reserved_bpb: dw 0
+        FATs: db 2
+        roots: dw 0
+        total_sects: dw 1
+        media: db 0x80
+        FAT_sects: dw 0
+        track_sects: dw 18
+        head_cnt: dw 2
+        hidden_sects: dd 0
+        large_sects: dd 0
 
-; set data segment:
-xor     ax, ax
-mov     ds, ax
+        sects_per_32: dd 0
+        flags: dw 0
+        fat_v: dw 0
+        root_clust: dd 0
+        fsinfo: dw 0
+        backup: dw 0
+        reserved_e: dd 0 
+                    dd 0 
+                    dd 0
+        drive_num: db 0x80
+        nt_flags: db 0
+        sig: db 0x28
+        volume: dd 0
+        volume_name: db "SMTH     "
+        sys: db "FAT32 "
 
-; set default video mode 80x25:
-mov     ah, 00h
-mov     al, 03h
-int     10h
+; We have to start somewhere, we use _start out of convention.
+_start:
+        ; We don't want to be disturbed.
+        cli
+        xor ax, ax
+        mov ss, ax
+        mov ds, ax
+        mov es, ax
+        
+        mov sp, 0x7C00
+        sti     
+        ; Now we're fine.
 
-;===================================
-; load the kernel at 0800h:0000h
-; 10 sectors starting at:
-;   cylinder: 0
-;   sector: 2
-;   head: 0
+   
+        ; Prepare ourselves for 80x25 text mode.
+        mov al, 3
+        int 0x10
 
-; BIOS passes drive number in dl,
-; so it's not changed:
+        ; Now we're going to load data from disk.
+        ; No filesystem yet.
+        mov ah, 2
+        mov al, 10
+        mov cx, 2
+        ; Some bad BIOS'es don't pass the disk number.
+        cmp dl, 0xFF
+        je from_floppy
+        cmp dl, 0x80
+        jge from_disk
+        mov dl, 0x80
+        jmp from_disk       
+        
+        ; Floppies are unreliable, so we have to try a few times.
+from_floppy:
+        mov bp, 5
+        jmp from_disk
+check_if_repeat:
+        sub bp, 1
+        cmp bp, 0
+        je spin
+from_disk: 
+        mov bx, 0x1000
+        mov es, bx
+        xor bx, bx
+        int 0x13
+        cmp dl, 0xFF
+        je check_for_errors
+check_for_errors:
+        jnc done_with_disk
+        cmp ah, 0
+        je done_with_disk
+        jmp spin 
 
-mov     ah, 02h ; read function.
-mov     al, 10  ; sectors to read.
-mov     ch, 0   ; cylinder.
-mov     cl, 2   ; sector.
-mov     dh, 0   ; head.
-; dl not changed! - drive number.
+done_with_disk:
+        jmp 0x1000:0x0000
 
-; es:bx points to receiving
-;  data buffer:
-mov     bx, 0AAAh   
-mov     es, bx  
-mov     bx, 0
+spin:
+        hlt
+inf:
+        jmp inf
 
-; read!
-int     13h
-jmp start
-
-times (510 - ($ - $$)) db 0
+times 510-($ - $$) db 0
 dw 0xAA55
 
 start:
@@ -88,22 +140,7 @@ ret
 __puts__next:
 	call putc
 	inc di
-	jmp puts    
-dat:      
-	SYS_help_text	db "A List of commands for COSA.", 10, 13
-			db "restart     |       restart the computer", 10, 13
-			db "splash      |       display the splash screen", 10, 13 
-			db "help        |       show this list", 10, 13, 0
-		
-	SYS_desc	db "Computer Operating System A.", 10, 13, "Kernel Version 0.1 Revision 0", 10, 13, "(C) Joshua Farmer 2023 lol", 10, 13, 0
-	SYS_keyboard 	db 0      
-	SYS_prompt	db "> ", 0
-	SYS_user	db "computer", 20h, 0
-	
-	INST_shutdown	db "restart", 0   
-	INST_desc	db "splash", 0 
-	INST_help	db "help", 0  
-dat_t:	 
+	jmp puts
 	
 getc:    
 	mov ah, 0
@@ -204,4 +241,20 @@ kernel:
 	
 	jmp kernel
 restart:
-	int 19h  
+	int 19h 
+	
+dat:      
+	SYS_help_text	db "A List of commands for COSA.", 10, 13
+			db "restart     |       restart the computer", 10, 13
+			db "splash      |       display the splash screen", 10, 13 
+			db "help        |       show this list", 10, 13, 0
+		
+	SYS_desc	db "Computer Operating System A.", 10, 13, "Kernel Version 0.1 Revision 0", 10, 13, "(C) Joshua Farmer 2023", 10, 13, 0
+	SYS_keyboard 	db 0      
+	SYS_prompt	db "> ", 0
+	SYS_user	db "computer", 20h, 0
+	
+	INST_shutdown	db "restart", 0   
+	INST_desc	db "splash", 0 
+	INST_help	db "help", 0  
+dat_t:	  
